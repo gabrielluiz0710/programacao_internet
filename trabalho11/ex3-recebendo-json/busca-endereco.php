97a73a5838180ea4370bb30688055609df30b382
Gabriel Luiz <gabriel.luiz@ufu.br>
2025-08-20 16:14:15 -0300

    ex3

--- trabalho11/ex3-recebendo-json/busca-endereco.php
+++ trabalho11/ex3-recebendo-json/busca-endereco.php
@@ -1,5 +1,5 @@
 <?php
-
+// Classe para organizar os dados do endereço
 class Endereco
 {
   public $rua;
@@ -15,7 +15,7 @@ class Endereco
 }
 
 $cep = $_GET['cep'] ?? '';
-
+// Com base no CEP, cria um objeto PHP com os dados do endereço
 if ($cep == '38400-100')
   $endereco = new Endereco('Av Floriano', 'Centro', 'Uberlândia');
 else if ($cep == '38400-200')
@@ -23,6 +23,7 @@ else if ($cep == '38400-200')
 else {
   $endereco = new Endereco('', '', '');
 }
-
+// Informa ao navegador que a resposta é do tipo JSON
 header('Content-type: application/json');
+// Converte o objeto PHP para o formato JSON e a envia como resposta
 echo json_encode($endereco);
