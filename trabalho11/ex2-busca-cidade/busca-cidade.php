c91599cf612498daf821a6f67c2b9459f336d2e8
Gabriel Luiz <gabriel.luiz@ufu.br>
2025-08-20 15:16:41 -0300

    ex1

--- trabalho11/ex2-busca-cidade/busca-cidade.php
+++ trabalho11/ex2-busca-cidade/busca-cidade.php
@@ -1,11 +1,14 @@
 <?php
-
+// Pega o valor do CEP que foi passado na URL, caso não exista usa uma string vazia
 $cep = $_GET['cep'] ?? '';
 
+// Se o valor for 38400-100, retorna Uberlândia
 if ($cep == '38400-100')
   echo 'Uberlândia';
+// Se não for, caso seja 38700-000, retorna Patos de Minas
 else if ($cep == '38700-000')
   echo 'Patos de Minas';
+// Caso contrário, retorna erro 404 e mensagem de erro
 else {
   http_response_code(404);
   echo "$cep is not available";
