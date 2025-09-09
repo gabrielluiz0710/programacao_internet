8d4dcd9dde0dcff71d433b973af3d8f3715b9a65
Gabriel Luiz <gabriel.luiz@ufu.br>
2025-09-08 22:30:40 -0300

    alter redirect

--- trabalho_final/public/central-user.php
+++ trabalho_final/public/central-user.php
@@ -54,7 +54,7 @@ requireLogin();
       </div>
 
       <div class="panel-actions">
-        <a href="criar-anuncio.html" class="action-button primary">
+        <a href="criar-anuncio.php" class="action-button primary">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="20" height="20">
             <path d="M11 11V5H13V11H19V13H13V19H11V13H5V11H11Z"></path>
           </svg>
